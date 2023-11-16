e1adde015b2bb50d2fb3ee3da3589799feea1486
seanwhite24 <150878866+seanwhite24@users.noreply.github.com>
2023-11-16 12:05:06 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,9 +1,9 @@
 /*   STEP 1   */
 export const leagueID = "982746154335027200"; // your league ID
 export const leagueName = "League of Scholar-Champions"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 30; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
